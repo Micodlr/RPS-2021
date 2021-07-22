da65e0d084a35b91310ba00283ae83e903704335
knuckleUp <dlreyesmico@gmail.com>
2021-07-22 15:58:55 -0700

    added computerPlay function and linked script.js to html

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function computerPlay() {
+    let picks = ["Rock", "Paper", "Scissors"];
+    let random = Math.floor(Math.random() * 3);
+    let randomPick = picks[random];
+    console.log(randomPick);
+}
+
