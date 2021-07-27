8b91c768c13a9819fb88e2df345bfa171613bb3a
knuckleUp <dlreyesmico@gmail.com>
2021-07-27 14:19:51 -0700

    added game function without using loop.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let playerScore = 0;
+let computerScore = 0;
+
+function lastScore(){
+    if(computerScore > playerScore){
+        console.log("you lose!");
+    }else if(playerScore > computerScore){
+        console.log("you win!");
+    }else {
+        console.log(draw);
+    }
+}
+
 function computerPlay() {
     let picks = ["Rock", "Paper", "Scissors"];
     let random = Math.floor(Math.random() * 3);
@@ -13,21 +26,69 @@ function playRound(playerSelection, computerSelection) {
         return alert("draw!");
         //win
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return alert("You win! Rock beats Scissors!");
+        playerScore++;
+        return alert("You win! Rock beats Scissors!"); 
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore++;
         return alert("You win! Paper beats Rock!");
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++;
         return alert("You win! Scissors beats Paper");
         //lose
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore++;
         return alert("You lose! Scissors beats Rock");
     } else if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore++;
         return alert("You lose! Paper beats Rock");
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore++;
         return alert("You lose! Scissors beats Paper");
     }
 }
-const playerSelection = "Rock";
+
+/* const playerSelection = "Rock";
 const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+console.log(playRound(playerSelection, computerSelection)); */
+
+function game() {
+    playerSelection = prompt("Rock, Paper or Scissors");
+    computerSelection = computerPlay();
+    
+    console.log(playRound(playerSelection, computerSelection));
+    console.log("computer score: ", computerScore);
+    console.log("player score: ",playerScore);
+
+    playerSelection = prompt("Rock, Paper or Scissors");
+    computerSelection = computerPlay();
+
+    console.log(playRound(playerSelection, computerSelection));
+    console.log("computer score: ", computerScore);
+    console.log("player score: ", playerScore);
+
+    playerSelection = prompt("Rock, Paper or Scissors");
+    computerSelection = computerPlay();
+
+    console.log(playRound(playerSelection, computerSelection));
+    console.log("computer score: ", computerScore);
+    console.log("player score: ", playerScore);
+
+    playerSelection = prompt("Rock, Paper or Scissors");
+    computerSelection = computerPlay();
+
+    console.log(playRound(playerSelection, computerSelection));
+    console.log("computer score: ", computerScore);
+    console.log("player score: ", playerScore);
+
+    playerSelection = prompt("Rock, Paper or Scissors");
+    computerSelection = computerPlay();
+
+    console.log(playRound(playerSelection, computerSelection));
+    console.log("computer score: ", computerScore);
+    console.log("player score: ", playerScore);
+
+    lastScore();
+    }
+game();
+
 
